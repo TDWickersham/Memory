7c72e99c8c3a6444bea047bd24feb44f8e2225b0
s179067 <s179067@SEA-8D0AB9>
2017-09-12 14:15:17 -0700

    memory

--- pointerArith/pointerArith.cpp
+++ pointerArith/pointerArith.cpp
@@ -22,10 +22,22 @@ void SumArr(int * arr, int size)
 	cout << sum << endl;
 }
 
+float arrayAvg(int * arr, int size)
+{
+	float average = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		average = average + *(arr + 1);
+	}
+	average = average / size;
+	return average;
+}
+
 int main()
 {
 	float * nums = new float[10];
 	int * numnum = new int[10];
+	int * avg = new int[10];
 
 	for (int i = 0; i < 10; ++i) { *(nums + i) = 0.1; }
 
@@ -36,8 +48,13 @@ int main()
 
 	SumArr(numnum, 10);
 
+	for (int c = 0; c < 10; ++c) { *(avg + c) = 2; }
+
+	cout << arrayAvg(avg, 10) << endl;
+
 	delete[] nums;
 	delete[] numnum;
+	delete[] avg;
 
 	system("pause");
 }
